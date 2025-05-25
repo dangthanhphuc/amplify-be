e3aa6da75b5f6ad5156340bf7b69d4bc7f2ff06b
dangthanhphuc2512 <dangthanhphuc2512@gmail.com>
2025-05-26 00:49:15 +0700

    Fix callback URL for local development in auth resource

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -28,7 +28,7 @@ export const auth = defineAuth({
       callbackUrls: [
         'https://5d6vfgww47.execute-api.us-east-1.amazonaws.com/dev/auth/getTokenByCode', // Production
         // `${process.env.API_GATEWAY}/callback`, // Sandbox
-        'http://localhost:3000/api/auth', // Local development for web
+        'http://localhost:3000/callback', // Local development for web
         'https://main.d36mfx1qa7bp2e.amplifyapp.com/callback', // Local development for web
       ],
       logoutUrls: ['http://localhost:3000/logout']

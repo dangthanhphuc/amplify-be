942e45a142f3cc238f862b386b9bcccfa16b5ad5
dangthanhphuc2512 <dangthanhphuc2512@gmail.com>
2025-05-27 07:57:55 +0700

    Change header for chat

--- amplify/functions/agents/chatWithAgent/handler.ts
+++ amplify/functions/agents/chatWithAgent/handler.ts
@@ -65,6 +65,9 @@ export const handler: APIGatewayProxyHandler = async (event) => {
         headers: {
           "Content-Type": "application/json",
           "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With",
+          "Cache-Control": "no-cache"
         },
         body: {
           response: completion,

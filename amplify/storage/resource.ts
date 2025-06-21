db6dac87335af50a4898487e0554d12b4069ea14
dangthanhphuc2512 <dangthanhphuc2512@gmail.com>
2025-06-21 09:21:01 +0700

    Refactor access permissions for knowledge base storage; consolidate user group permissions.

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,6 +3,11 @@ import { defineStorage } from "@aws-amplify/backend";
 export const storageForProject = defineStorage({
     name: "storageForProject",
     access: (allow) => ({
+        'knowledge-base/*': [
+            allow.guest.to(['read', 'write', 'delete']),
+            allow.authenticated.to(['read', 'write', 'delete']),
+            allow.groups(["USERS"]).to(['read', 'write', 'delete']),
+        ],
         'profile-pictures/{entity_id}/*': [
             allow.guest.to(['read']),
             allow.authenticated.to(['read']),
@@ -11,7 +16,6 @@ export const storageForProject = defineStorage({
         ],
         'knowledge-base/{entity_id}/*': [
             allow.entity("identity").to(['read', 'write', 'delete']),
-            allow.groups(["USERS"]).to(['read', 'write', 'delete']),
         ],
         'public-images/*': [
             allow.guest.to(['read']),
